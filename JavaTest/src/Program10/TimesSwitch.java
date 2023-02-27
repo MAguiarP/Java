7b3e6fba4bd439cfa692db89feac5050b9876eca
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2023-02-27 15:06:44 -0300

    add

--- JavaTest/src/Program10/TimesSwitch.java
+++ JavaTest/src/Program10/TimesSwitch.java
@@ -5,6 +5,8 @@
  */
 package Program10;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author mathe
@@ -40,6 +42,11 @@ public class TimesSwitch extends javax.swing.JFrame {
 
         jCBResposta.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jCBResposta.setText("Veja a data de fundação de seu time.");
+        jCBResposta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCBRespostaActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -71,6 +78,44 @@ public class TimesSwitch extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jCBRespostaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCBRespostaActionPerformed
+       switch(jCBTimes.getSelectedIndex()){
+            case 0:
+                JOptionPane.showMessageDialog(null, "26 de Agosto de 1914"); break;
+            case 1:
+                JOptionPane.showMessageDialog(null, "1 de Setembro de 1910"); break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "14 de Abril de 1912"); break; 
+            case 3:
+                JOptionPane.showMessageDialog(null, "27 de Janeiro de 1930"); break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "1 de Julho de 1894"); break;    
+            case 5:
+                JOptionPane.showMessageDialog(null, "21 de Julho de 1902"); break;    
+            case 6:
+                JOptionPane.showMessageDialog(null, "21 de Agosto de 1894"); break;    
+            case 7:
+                JOptionPane.showMessageDialog(null, "17 de Novembro de 1895"); break;    
+            case 8:
+                JOptionPane.showMessageDialog(null, "12 de Outubro de 1909"); break;    
+            case 9:
+                JOptionPane.showMessageDialog(null, "25 de Março de 1908"); break;    
+            case 10:
+                JOptionPane.showMessageDialog(null, "26 de Março de 1924"); break;    
+            case 11:
+                JOptionPane.showMessageDialog(null, "02 de Janeiro de 1921"); break;
+            case 12:
+                JOptionPane.showMessageDialog(null, "04 de Abril de 1909"); break;
+            case 13:
+                JOptionPane.showMessageDialog(null, "15 de Setembro de 1903"); break;
+            case 14:
+                JOptionPane.showMessageDialog(null, "13 de Maio de 1905"); break;
+                         
+            default:
+                JOptionPane.showMessageDialog(null, ""); break;
+       }
+    }//GEN-LAST:event_jCBRespostaActionPerformed
 
     /**
      * @param args the command line arguments
