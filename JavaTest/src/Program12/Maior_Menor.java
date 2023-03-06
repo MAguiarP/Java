198ac1122c58638fbdd13f9bc1cde0a4dc3e908d
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2023-03-06 11:16:10 -0300

    não terminado
    erro

--- JavaTest/src/Program12/Maior_Menor.java
+++ JavaTest/src/Program12/Maior_Menor.java
@@ -5,6 +5,8 @@
  */
 package Program12;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author mathe
@@ -34,12 +36,12 @@ public class Maior_Menor extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
+        jTPrimeiroNum = new javax.swing.JTextField();
+        jTSegundoNum = new javax.swing.JTextField();
+        jTTerceiroNum = new javax.swing.JTextField();
         jBCalcular = new javax.swing.JButton();
         jBLimpar = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jBSair = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Maior/Menor número.");
@@ -47,26 +49,43 @@ public class Maior_Menor extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Selecione a opção desejada!");
 
+        GrupoBotao.add(jRMaiorNum);
         jRMaiorNum.setText("Maior Número");
 
+        GrupoBotao.add(jRMenorNum);
         jRMenorNum.setText("Menor Número");
 
-        jLabel2.setText("jLabel2");
+        jLabel2.setText("1° Número");
 
-        jLabel3.setText("jLabel3");
+        jLabel3.setText("2° Número");
 
-        jLabel4.setText("jLabel4");
+        jLabel4.setText("3° Número");
 
         jBCalcular.setForeground(new java.awt.Color(0, 51, 255));
         jBCalcular.setText("Calcular");
+        jBCalcular.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBCalcularActionPerformed(evt);
+            }
+        });
 
         jBLimpar.setBackground(new java.awt.Color(204, 204, 204));
         jBLimpar.setForeground(new java.awt.Color(204, 204, 204));
         jBLimpar.setText("Limpar");
+        jBLimpar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBLimparActionPerformed(evt);
+            }
+        });
 
-        jButton3.setBackground(new java.awt.Color(0, 0, 0));
-        jButton3.setForeground(new java.awt.Color(255, 0, 0));
-        jButton3.setText("Sair");
+        jBSair.setBackground(new java.awt.Color(0, 0, 0));
+        jBSair.setForeground(new java.awt.Color(255, 0, 0));
+        jBSair.setText("Sair");
+        jBSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBSairActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -77,36 +96,36 @@ public class Maior_Menor extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
-                .addGap(18, 18, 18)
+                .addGap(26, 26, 26)
+                .addComponent(jRMenorNum)
+                .addGap(40, 40, 40)
+                .addComponent(jRMaiorNum)
+                .addGap(0, 82, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addGap(18, 18, 18)
-                                .addComponent(jTextField2))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel4)
-                                .addGap(18, 18, 18)
-                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jLabel3)
                         .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jBLimpar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addContainerGap(27, Short.MAX_VALUE))
+                        .addComponent(jTSegundoNum))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                .addComponent(jRMenorNum)
-                                .addGap(40, 40, 40)
-                                .addComponent(jRMaiorNum))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(18, 18, 18)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jBCalcular)))
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTTerceiroNum))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTPrimeiroNum)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jBCalcular, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jBSair, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jBLimpar, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -120,23 +139,68 @@ public class Maior_Menor extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTPrimeiroNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBCalcular, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTSegundoNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBLimpar))
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3))
+                    .addComponent(jTTerceiroNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jBSair))
                 .addContainerGap(29, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jBSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBSairActionPerformed
+        System.exit(0);   
+    }//GEN-LAST:event_jBSairActionPerformed
+
+    private void jBLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBLimparActionPerformed
+         jTPrimeiroNum.setText(null);
+         jTSegundoNum.setText(null);
+         jTTerceiroNum.setText(null);
+         GrupoBotao.clearSelection();
+         jTPrimeiroNum.requestFocus();
+    }//GEN-LAST:event_jBLimparActionPerformed
+
+    private void jBCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCalcularActionPerformed
+        int Num1;
+        int Num2;
+        int Num3;
+        
+        Num1 = Integer.parseInt(jTPrimeiroNum.getText());
+        Num2 = Integer.parseInt(jTSegundoNum.getText());
+        Num3 = Integer.parseInt(jTTerceiroNum.getText());
+      
+        double total;
+        
+        total = Num1 =  Num2 = Num3;
+        
+        if(jRMaiorNum.isSelected()){
+            if(total >=50 ) {
+                JOptionPane.showMessageDialog(null,total + "É o menor Número");
+            }else if(total >=50){
+                JOptionPane.showMessageDialog(null,total + "É o Maior Número");
+            }
+         
+      }
+            if(jRMenorNum.isSelected()){
+            if(total <10 ) {
+                JOptionPane.showMessageDialog(null,total + "É o menor Número");
+            
+            }else{
+                JOptionPane.showMessageDialog(null,total + "É o diferente Número");
+            }
+         
+      }
+      
+    }//GEN-LAST:event_jBCalcularActionPerformed
 
     /**
      * @param args the command line arguments
@@ -177,15 +241,15 @@ public class Maior_Menor extends javax.swing.JFrame {
     private javax.swing.ButtonGroup GrupoBotao;
     private javax.swing.JButton jBCalcular;
     private javax.swing.JButton jBLimpar;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jBSair;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JRadioButton jRMaiorNum;
     private javax.swing.JRadioButton jRMenorNum;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTPrimeiroNum;
+    private javax.swing.JTextField jTSegundoNum;
+    private javax.swing.JTextField jTTerceiroNum;
     // End of variables declaration//GEN-END:variables
 }
