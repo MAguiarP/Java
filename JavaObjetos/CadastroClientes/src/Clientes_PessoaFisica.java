775143176616b433248869085b543a9f1e35f337
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2023-04-11 14:39:04 -0300

    add

---
+++ JavaObjetos/CadastroClientes/src/Clientes_PessoaFisica.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mathe
+ */
+public class Clientes_PessoaFisica extends Clientes{
+    //Atributos criados ppara nome, endereço, email e telefone.
+    String cpf , rg;
+    
+    //métodos que permitem atribuir dados aos atributos
+    public void setcpf(String scpf){
+        this.cpf = scpf;
+    }
+    
+     public void setrg(String srg){
+        this.rg = srg;
+    }
+     
+    //método que permite recuperar o conteúdo dos atributos
+     public String retorna_cpf(){
+         return cpf;
+     }
+     
+      public String retorna_rg(){
+         return rg;
+     }
+     
+}
