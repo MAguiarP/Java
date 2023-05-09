e5c20902d7e12f8e8b9acb5966b53635055e4458
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2023-05-09 14:40:00 -0300

    addd

--- JavaMod2/ControledeEstoque/src/controledeestoque/dao/ConnectionFactory.java
+++ JavaMod2/ControledeEstoque/src/controledeestoque/dao/ConnectionFactory.java
@@ -9,5 +9,24 @@ import java.sql.*;
  * @author mathe
  */
 public class ConnectionFactory {
+    private static final String URL = "jdbc:postgresql://localhost:5432/DBControledeEstoque";
+    private static String USER = "UserControleEstoque";
+    private static final String PASS = "123456";
     
+
+
+    public static Connection getConnection()
+    {
+        try
+        {
+            System.out.print("Conectando ao banco de dados!");
+            return DriverManager.getConnection(URL, USER, PASS);                   
+        }
+        
+        catch(SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        
+    }
 }
