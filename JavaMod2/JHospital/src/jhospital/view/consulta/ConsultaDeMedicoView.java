a7e6f3a50457692dd7c993b7e014606bb56d67de
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-06-20 12:01:25 -0300

    adds

--- JavaMod2/JHospital/src/jhospital/view/consulta/ConsultaDeMedicoView.java
+++ JavaMod2/JHospital/src/jhospital/view/consulta/ConsultaDeMedicoView.java
@@ -98,6 +98,11 @@ public class ConsultaDeMedicoView extends javax.swing.JPanel {
         );
 
         jButton1.setText("Consultar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Fechar");
 
@@ -157,6 +162,10 @@ public class ConsultaDeMedicoView extends javax.swing.JPanel {
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
